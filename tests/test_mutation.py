2b6c85ce819cd3bad783bd079fa52a3c1a49e732
Serdar Kadioglu <serdar.kadioglu@fmr.com>
2023-07-16 19:21:29 -0400

    update

--- tests/test_mutation.py
+++ tests/test_mutation.py
@@ -1,18 +1,16 @@
 import os
-import numpy as np
-
-from alns import ALNS
 from alns.accept import *
 from alns.select import *
 from alns.stop import *
 
-from balans.base_state import _State
-from balans.base_instance import _Instance
-from balans.mutation import mutation_25
-from balans.repair.repair import repair
+from balans.destroy import DestroyOperators
+from balans.repair import RepairOperators
+from balans.solver import Balans
 from balans.utils import Constants
 from tests.test_base import BaseTest
-from mabwiser.mab import LearningPolicy
+
+from mabwiser.mab import LearningPolicy, NeighborhoodPolicy
+
 
 TEST_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = TEST_DIR + os.sep + ".." + os.sep
@@ -22,46 +20,25 @@ class MutationTest(BaseTest):
 
     def test_mutation(self):
 
-        # Set RNG
-        np.random.seed(Constants.default_seed)
+        # Input
+        instance = "neos-5140963-mincio.mps.gz"
+        instance_path = os.path.join(ROOT_DIR, Constants.DATA_DIR, instance)
 
-        # ALNS
-        alns = ALNS(np.random.RandomState(Constants.default_seed))
-
-        # Operators
-        alns.add_destroy_operator(mutation_25)
-        alns.add_repair_operator(repair)
-
-        # Create instance from mip file
-        instance_name = "neos-5140963-mincio.mps.gz"
-        instance_path = os.path.join(ROOT_DIR, "data", instance_name)
-        instance = _Instance(instance_path)
-
-        # Initial solution
-        initial_var_to_val, initial_obj_val = instance.solve(gap=0.50, time=30)
-        print(">>> START objective:", initial_obj_val)
-
-        # Initial state with the initial solution
-        initial_state = _State(instance, initial_var_to_val, initial_obj_val)
-
-        # Bandit selector
-        select = MABSelector(scores=[5, 2, 1, 0.5],
-                             num_destroy=1,
-                             num_repair=1,
-                             learning_policy=LearningPolicy.EpsilonGreedy(epsilon=0.15))
-
-        # Accept criterion
+        # Parameters
+        seed = Constants.default_seed
+        destroy_ops = [DestroyOperators.Mutation]
+        repair_ops = [RepairOperators.Repair]
+        selector = MABSelector(scores=[5, 2, 1, 0.5], num_destroy=5, num_repair=1,
+                               learning_policy=LearningPolicy.EpsilonGreedy(epsilon=0.15))
         accept = HillClimbing()
+        stop = MaxIterations(5)
 
-        # Stop condition
-        stop = MaxIterations(100)
+        # Solver
+        balans = Balans(destroy_ops, repair_ops, selector, accept, stop, seed)
 
         # Run
-        result = alns.iterate(initial_state, select, accept, stop)
-
-        # Result
-        final_obj_value = result.best_state.objective()
-        print(">>> FINISH objective:", final_obj_value)
+        result = balans.solve(instance_path)
+        print("Best solution:", result.best_state.objective())
 
         # Assert
-        self.assertTrue(self.is_better(initial_obj_val, final_obj_value, instance.sense))
+        self.assertIsBetter(balans.initial_obj_val, result.best_state.objective(), balans.instance.sense)
