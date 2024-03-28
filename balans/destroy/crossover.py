0f09bd9e70f9cb6b7a4457e6915cc2ecfdfc7e83
Serdar Kadioglu <serdar.kadioglu@fmr.com>
2024-03-28 05:03:07 -0400

    update

--- balans/destroy/crossover.py
+++ balans/destroy/crossover.py
@@ -55,37 +55,6 @@ def crossover(current: _State, rnd_state) -> _State:
 
 
 # def crossover2(current: _State, rnd_state) -> _State:
-#     #  Take two random*** (CURRENTLY LAST TWO SOLUTION) solutions.
-#     #  If a DISCRETE variable x_inc = x_inc2, do not change it.
-#     #  Otherwise, put it to the destroy set.
-#     #  Send the destroy set to base_instance.
-#     print("\t Destroy current objective:", current.obj_val)
-#     next_state = copy.deepcopy(current)
-#     next_state.reset_solve_settings()
-#
-#     # Static features from the instance
-#     discrete_indexes = current.instance.discrete_indexes
-#     random_sol = current.instance.random_index_to_val
-#
-#     print("Random Solution Crossover:", random_sol)
-#     # TODO where is previous_index was updated before landing here? It is initialized in State creation but then not updated
-#     #  If a variable x_inc = x_inc2, do not change it.
-#     indexes_with_same_value = [i for i in discrete_indexes if
-#                                math.isclose(current.previous_index_to_val[i], next_state.index_to_val[i])]
-#
-#     # Else potentially change it
-#     crossover_set = set([i for i in discrete_indexes if i not in indexes_with_same_value])
-#     print("\t Previous Index to val:", next_state.index_to_val)
-#
-#     print("\t Destroy set:", crossover_set)
-#     return _State(next_state.instance,
-#                   next_state.index_to_val,
-#                   next_state.obj_val,
-#                   destroy_set=crossover_set,
-#                   previous_index_to_val=current.index_to_val)
-#
-
-# def crossover3(current: _State, rnd_state) -> _State:
 #     #  Take one random and current incumbent solutions.
 #     #  If a DISCRETE variable x_inc = x_rand, do not change it.
 #     #  Otherwise, put it to the destroy set.
@@ -102,7 +71,7 @@ def crossover(current: _State, rnd_state) -> _State:
 #
 #     #  If a variable x_inc = x_rand, do not change it.
 #     indexes_with_same_value = [i for i in discrete_indexes if
-#                                np.isclose(random_sol[i], next_state.index_to_val[i])]
+#                                math.isclose(random_sol[i], next_state.index_to_val[i])]
 #
 #     # Else potentially change it
 #     crossover_set = set([i for i in discrete_indexes if i not in indexes_with_same_value])
@@ -113,5 +82,38 @@ def crossover(current: _State, rnd_state) -> _State:
 #                   next_state.index_to_val,
 #                   next_state.obj_val,
 #                   destroy_set=crossover_set)
+
+# def crossover3(current: _State, rnd_state) -> _State:
+#     #  Take two random*** (CURRENTLY LAST TWO SOLUTION) solutions.
+#     #  If a DISCRETE variable x_inc = x_inc2, do not change it.
+#     #  Otherwise, put it to the destroy set.
+#     #  Send the destroy set to base_instance.
+#     print("\t Destroy current objective:", current.obj_val)
+#     next_state = copy.deepcopy(current)
+#     next_state.reset_solve_settings()
+#
+#     # Static features from the instance
+#     discrete_indexes = current.instance.discrete_indexes
+#     random_sol = current.instance.random_index_to_val
+#
+#     print("Random Solution Crossover:", random_sol)
+#     #  If a variable x_inc = x_inc2, do not change it.
+#     # TODO This probably has a BUG. Where was previous_index updated before coming here?
+#     # Previous solution is only initialized in the root note in State creation but then never updated
+#     indexes_with_same_value = [i for i in discrete_indexes if
+#                                math.isclose(current.previous_index_to_val[i], next_state.index_to_val[i])]
+#
+#     # Else potentially change it
+#     crossover_set = set([i for i in discrete_indexes if i not in indexes_with_same_value])
+#     print("\t Previous Index to val:", next_state.index_to_val)
+#
+#     print("\t Destroy set:", crossover_set)
+#     return _State(next_state.instance,
+#                   next_state.index_to_val,
+#                   next_state.obj_val,
+#                   destroy_set=crossover_set,
+#                   previous_index_to_val=current.index_to_val)
+#
+
 
 
