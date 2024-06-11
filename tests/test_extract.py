53bae80edce89d7ced0553e1469b3467022ce2f3
Serdar Kadioglu <serdar.kadioglu@fmr.com>
2024-06-11 17:37:36 -0400

    update

---
+++ tests/test_extract.py
@@ -0,0 +1,11 @@
+import os
+from balans.utils import Constants
+from tests.test_base import BaseTest
+
+
+class IndexExtractionTest(BaseTest):
+
+    def test_extract(self):
+
+        self.assertTrue(False)
+
